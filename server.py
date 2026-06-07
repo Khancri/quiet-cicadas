8300a11c8db7ab4c4f567c5c304f49e47299e842
Jade Khan <joeykhan0106@gmail.com>
2026-06-06 23:09:35 -0400

    i will make this and i will fucking love it

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='.')
+CORS(app)
+
+@app.route('/')
+def main():
+    return send_from_directory('client.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2994)
