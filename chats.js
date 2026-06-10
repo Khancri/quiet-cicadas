1773a18ce2bcc0295dd91211663e211ae69d06f4
Jade Khan <joeykhan0106@gmail.com>
2026-06-10 03:14:45 -0400

    I MADE THE CHAT THING YAHOO

--- chats.js
+++ chats.js
@@ -7,20 +7,13 @@ const daata = {
             '🎉': ['khancri', 'l.', 'giitar_ruff'],
             '⛷': ['khancri']
         }
-    },
-    'uuid2': {
-        date: '2026-06-09T11:11:53.518Z',
-        content: 'I do not like big butts and I do not lie',
-        user: 'giitar_ruff'
     }
 }
-console.log(new Date().toUTCString());
-function renderMessages(data) {
-    const div = document.createElement('div');
-    div.classList.add('messages');
-    for (const [key, value] of Object.entries(data)) {
-        const message = document.createElement('div');
+function createMessage(data, id) {
+    const message = document.createElement('div');
         message.classList.add('message');
+
+        message.dataset.id = id;
         
         const pfp = document.createElement('div');
         pfp.classList.add('avatar');
@@ -35,22 +28,67 @@ function renderMessages(data) {
         content.appendChild(metadata);
 
         const usernameEl = document.createElement('b');
-        usernameEl.classList.add('username'); usernameEl.innerText = value.user;
+        usernameEl.classList.add('username'); usernameEl.innerText = data.user;
         const dataEl = document.createElement('span');
-        const date = new Date(value.date);
-        console.log(date.valueOf());
+        const date = new Date(data.date);
         dataEl.classList.add('date'); dataEl.innerText =  date.toLocaleString();
         metadata.appendChild(usernameEl); metadata.appendChild(dataEl);
 
         const messageContent = document.createElement('span');
-        messageContent.classList.add('messageContent'); messageContent.innerText = value.content;
-
+        messageContent.classList.add('messageContent'); messageContent.innerText = data.content;
         content.appendChild(messageContent);
-        div.appendChild(message);
-    }
-    document.body.appendChild(div);
-}
-document.addEventListener('DOMContentLoaded', () => {
 
+        const reactionRow = document.createElement('div');
+        reactionRow.className = 'reaction-row';
+        content.appendChild(reactionRow);
+        if (data.reactions) {
+            for (const [emoji, array] of Object.entries(data.reactions)) {
+                const reactionEl = document.createElement('div'); reactionEl.className = 'reaction';
+                reactionEl.innerHTML = `<span class="emoji">${emoji}</span>:<span class="counter">${array.length}</span>`
+                reactionRow.appendChild(reactionEl);
+            }
+        }
+        return message;
+
+}
+console.log(new Date().toUTCString());
+function renderMessages(data) {
+const sorted = Object.entries(data).sort((a, b) => new Date(a[1].date) - new Date(b[1].date));
+    const div = document.getElementById('messages')
+    for (const [key, value] of sorted) {
+        const message = document.querySelector(`[data-id=\"${key}\"]`)
+        if (message === null) {
+            div.appendChild(createMessage(value, key));
+        }
+    }
+    div.scrollTop = div.scrollHeight;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+const messageInput = document.getElementById('message-input');
+messageInput.addEventListener('keydown', async (e) => {
+    if (e.key === 'Enter') {
+        if (messageInput.value.trim() == '') return;
+        const res = await fetch('/message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({message: messageInput.value.trim()})
+        });
+        messageInput.value = ''; 
+    }
+});
+    checkUser();
     renderMessages(daata);
-})
+    getMessages();
+});
+async function checkUser() {
+    const username = (await (await fetch('/me')).json()).username
+    document.getElementById('current-user-username').innerText = username;
+}
+async function getMessages() {
+    const data = await (await fetch('/messages')).json();
+    renderMessages(data);
+}
--- server.py
+++ server.py
@@ -1,23 +1,27 @@
 from flask import Flask, jsonify, request, send_from_directory, session
+from datetime import datetime
 from flask_cors import CORS
+import flask_socketio
+import uuid
 import bcrypt
 import json; import os;
 PROFILE_FILE = 'profiles.json'
 
-
 app = Flask(__name__, static_folder='.')
+socketio = flask_socketio.SocketIO(app, cors_allowed_origins="*")
 app.secret_key = 'R5m9SAXRxLwERafXLj5hqW4qru98NhWz'
 CORS(app)
-
-def load():
-    if not os.path.exists(PROFILE_FILE):
+#region JSON
+def load(file):
+    if not os.path.exists(file):
         return {}
-    with open(PROFILE_FILE) as f:
+    with open(file) as f:
         return json.load(f)
 
-def save(todos):
-    with open(PROFILE_FILE, 'w') as f:
+def save(file, todos):
+    with open(file, 'w') as f:
         json.dump(todos, f, indent=4)
+#endregion
 
 @app.route('/chat')
 def chat():
@@ -46,21 +50,40 @@ def profile():
 
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
-
-
     profileObj = {'username': username, 'password': hashed.decode()}
-    file = load()
+    file = load('profiles.json')
     file[username] = profileObj
-    save(file)
+    save('profiles.json' , file)
     session['username'] = username
     return '', 204
 
 @app.route('/profile/exists/<string:username>', methods=['GET'])
 def exising_username(username: str):
-    if username in load().keys():
+    if username in load('profiles.json').keys():
         return jsonify({'ok': True})
     return jsonify({'ok': False})
     
+@app.route('/message', methods=['POST'])
+def post_message():
+    info = request.json
+    message_obj = {
+        'user': session['username'],
+        'content': info['message'],
+        'date': datetime.now().isoformat()
+    }    
+    file = load('messages.json'); 
+    hash = str(uuid.uuid4())
+    file[hash] = message_obj
+    save('messages.json', file)
+    socketio.emit('new_message', {hash: message_obj})
+    return '', 204
+
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    data = load('messages.json')
+    sorted_msgs = sorted(data.items(), key=lambda x: x[1]['date'], reverse=True)
+    recent = dict(sorted_msgs[:10])
+    return jsonify(recent)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2994)
+    socketio.run(app, host='0.0.0.0', port=2994)
